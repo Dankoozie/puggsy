9d3dbd53c8ad3dddb3d7ba120365a59b66d94f0e
Daniel <puggsy@puggsy.net>
2016-01-04 00:34:03 +0000

    remove old stuff

--- otp.py
+++ otp.py
@@ -65,12 +65,6 @@ def Save_blockids(padid,fileid,bids):
 
 class Otp_coder:
 
-    def savestate(self,cid):
-        sf = open(OTPDir + str(cid) + "/state.txt","w")
-        sf.write(str(cid) + "\n") # Default file pair in use
-        sf.write(str(cid) + ":" + str(self.in_offset) + ":" + str(self.out_offset) + "\n")
-        sf.close()
-
     def loadpickle(self,fid):
         #Outgoing pad
         lto = open(OTPDir +  str(self.contactid) + "/" + "blocks-out." + str(fid),'rb')
@@ -82,18 +76,8 @@ class Otp_coder:
         self.blocks_in = pickle.load(lfrom)
         lfrom.close()
 
-
     def savepickle(self,cid):
         sf = open(OTPDir + str(cid) + "/state.txt","w")
-
-
-    def loadpads(self,cid):
-        f = open(OTPDir + str(cid) + "/in.0","rb")
-        o = open(OTPDir + str(cid) + "/out.0","rb")
-        self.in_contents = f.read()
-        self.out_contents = o.read()
-        f.close()
-        o.close()
 
     def stats(self):
         pused_in = (self.in_offset / len(self.in_contents))*100
@@ -103,66 +87,7 @@ class Otp_coder:
     
     def __init__(self,contact_id):
         functional = False
-        self.in_file_list = {}
-        self.in_file_active = 0 
-        self.in_contents = b""
-        self.in_offset = 0
-        self.out_file_list = {}
-        self.out_file_active = 0
-        self.out_contents = b""
-        self.out_offset = 0
-        self.loadpads("0")
         self.contactid = contact_id
-
-    
-
-    def encrypt(self,ba_str,file = 0):
-        send_offset = self.out_offset  #Offset value for sending to other party (initial offset of message)    
-        cs = 0
-        el = bytearray()
-
-        if((len(ba_str) + self.out_offset) > len(self.out_contents) + 4):
-            print("Out of pad material")
-            return(('',''))
-        
-        for a in range(0,len(ba_str)):
-            el.append(ord(ba_str[a]) ^ self.out_contents[a+self.out_offset])
-
-        self.out_offset = self.out_offset + len(ba_str)
-
-        
-        xorlong = unpack(">L",self.out_contents[self.out_offset:self.out_offset+4])
-
-        cs = (crc32(bytes(ba_str,'utf-8')) ^ xorlong[0])        
-        self.out_offset = self.out_offset + 4
-        self.savestate(0)
-        return (pack(">QQQL",0,0,send_offset,cs), el)
-
-    def decrypt(self,ba_str,bql):
-        if(len(bql) != 13):
-            print("Invalid header")
-            return (False,"")
-            
-        (file,offset,crc) = unpack(">QQQL",bql)
-
-        if(offset < self.in_offset):
-            print("Offset less than internal state / Possible pad reuse")
-
-        
-        d_str = bytearray()
-        for a in ba_str:
-            d_str.append(a ^ self.in_contents[offset])
-            offset = offset + 1
-
-
-        xorlong = unpack(">L",self.in_contents[offset:offset+4])
-        
-        if(crc32(d_str) == crc ^ xorlong[0]):
-            self.in_offset = offset + 4
-            self.savestate(0)
-            return (True,d_str)
-        else:
-            return(False,"")
 
     def getblok(self,padid,blokid,direction = 0):
         if(direction == 0): pdir = "/out."
